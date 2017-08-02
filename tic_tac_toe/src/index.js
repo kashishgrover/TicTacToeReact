615b7abe937df549259e4cedc5b4923348c7287d
Kashish Grover <kashish@hashnode.com>
2017-08-02 14:51:40 +0530

    Added handlClick function

--- tic_tac_toe/src/index.js
+++ tic_tac_toe/src/index.js
@@ -3,26 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          value: null,
-        };
-      }
+  constructor() {
+      super();
+      this.state = {
+        value: null,
+      };
+    }
 
   render() {
-    return (
+      return (
         //Note the syntax for ES6's arrow function here
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
+        <button className="square" onClick={() => this.props.onClick()}>
+          {this.props.value}
+        </button>
+      );
   }
 }
 
 class Board extends React.Component {
+
+  constructor() {
+      super();
+      this.state = {
+          squares: Array(9).fill(null),
+      }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
